87ee24aee080adcb7caf32ef8d6ee505aad56b75
Bill Ayers <BillA@flosim.com>
2021-11-09 15:56:33 +0000

    Moved grid to separate class

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -6,18 +6,8 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
-            int[,] grid = new int[9,9];
-            for(int j=0;j<9;j++)
-                for(int i=0;i<9;i++)
-                    grid[i,j] = i;
-
-            /* print out the grid */
-            for (int j = 0; j < 9; j++)
-                for (int i = 0; i < 9; i++)
-            {
-                Console.Write(grid[i,j] + " ");
-            }
-            Console.WriteLine();
+            var grid = new Grid();
+            Console.WriteLine(grid.ToString());
         }
     }
 }
